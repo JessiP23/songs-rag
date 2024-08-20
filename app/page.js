a6cab91ab4b3e939413b8c563f32b6d61c3ef9d1
JessiP23 <jessi316866@gmail.com>
2024-08-19 21:03:45 -0400

    frontend completed

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { Box, Stack } from "@mui/material";
+import { Box, Button, Stack, TextField } from "@mui/material";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -12,7 +12,7 @@ export default function Home() {
   ])
   const [message, setMessage] = useState('')
   const sendMessage = async() => {
-    setMessages=((messages) => [
+    setMessages((messages) => [
       ...messages,
       {role: 'user', content: message},
       {role: 'assistant', content: ''}
@@ -62,14 +62,24 @@ export default function Home() {
       alignItems="center"
     >
       <Stack direction="column" width="500px" height="700px" border="1px solid black" p={2} spacing={3}>
+        <Stack direction='column' spacing={2} flexGrow={1} overflow='auto' maxHeight="100%">
         {
-          messages.map((message, index) => {
+          messages.map((message, index) => (
             <Box key={index} display="flex" justifyContent={message.role === 'assistant' ? 'flex-start' : 'flex-end'}>
               <Box bgcolor={message.role === 'assistant' ? 'primary.main' : 'secondary.main'} color="white" borderRadius={16} p={3}>       
                 {message.content} 
               </Box>
             </Box>
-          })}
+          ))}
+          </Stack>
+          <Stack direction='row' spacing={2}>
+            <TextField label="Message" fullWidth value={message} onChange={(e) => {
+              setMessage(e.target.value)
+            }} />
+            <Button variant="contained" onClick={sendMessage}>
+              Send
+            </Button>
+          </Stack>
       </Stack>
     </Box>
   );
