0964e5347464f1cfd9b1d161bf78699a69e8577b
JessiP23 <jessi316866@gmail.com>
2024-08-22 16:00:10 -0400

    adde start to add animation when hovering

--- components/StarRating.js
+++ components/StarRating.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StarIcon from '@mui/icons-material/Star';
 
 export default function StarRating() {
+
+    const [rating, setRating] = useState(null);
+    const [rateColor, setColor] = useState(null);
+
     return (
         <>
-            {[...Array(5)].map(star => {
+            {[...Array(5)].map((star, index) => {
+                const currentRate = index + 1
                 return (
-                    <StarIcon />
+                    <>
+                        <label>
+                            <StarIcon sx={{ fontSize: '80px', margin: '8px'}} />
+                        </label>
+                        
+                    </>
                 )
             })}
         </>
